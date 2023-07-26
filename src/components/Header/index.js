3fa53979acd329be3c51a8a0f4d28969e03b87d5
sukanya316 <61020902+sukanya316@users.noreply.github.com>
2023-07-26 11:14:57 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,13 +5,13 @@ const Header=()=>{
         <div className='header-container'>
             <img src="https://i.imgur.com/0ZBZ5hz.png" alt="file" className='file-img'/>
             <ul className='header-items-container'>
-                <li>Home</li>
-                <li>Services</li>
-                <li>About Us</li>
-                <li>Order Company</li>
-                <li>Apply for Tax Rebate</li>
-                <li>Blog</li>
-                <li>Contact</li>
+                <li className="header-nav-item">Home</li>
+                <li className="header-nav-item">Services</li>
+                <li className="header-nav-item">About Us</li>
+                <li className="header-nav-item">Order Company</li>
+                <li className="header-nav-item">Apply for Tax Rebate</li>
+                <li className="header-nav-item">Blog</li>
+                <li className="header-nav-item">Contact</li>
             </ul>
         </div>
     )
